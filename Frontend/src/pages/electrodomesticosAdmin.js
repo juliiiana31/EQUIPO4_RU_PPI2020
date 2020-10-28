3edaac19febd0e9b429e8aa3200978163a557eb2
John Mario <66332602+JohnyGat@users.noreply.github.com>
2020-10-27 21:47:35 -0600

    Revisando los botones faltantes

--- Frontend/src/pages/electrodomesticosAdmin.js
+++ Frontend/src/pages/electrodomesticosAdmin.js
@@ -62,7 +62,11 @@ class ElectrodomesticosAdmin extends React.Component {
 
               <button class="btn btn-success m-3 px-4">Eiminar</button>
             </div>
+<<<<<<< Codesandbox
+            <a type="submit" class="btn btn-success m-3 px-4" href="/">
+=======
             <a type="submit" class="btn btn-success m-3 px-4" href="/totalpaneles">
+>>>>>>> GitHub
                Regresar
             </a>
             <a type="submit" class="btn btn-success m-3 px-4" href="/totalpaneles">
