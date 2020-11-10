24d9d640ec73228047fd01bb86c1f159c0491310
Juliana Gómez <60723362+juliiiana31@users.noreply.github.com>
2020-11-10 15:50:24 -0500

    cambios en el index con los servicios
    paneles solares
    usuarios
    electrodomesticos

--- Back/index.js
+++ Back/index.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const app=express();
-// const routes=require('./routes/routes'); // defino todos los middleware de los modulo
-// const modulos=require('./routes/modulos'); 
+//const electrodomesticos=require('./routes/electrodomesticos'); // defino todos los middleware de los modulo
+//const usuario=require('./routes/usuario'); 
 const paneles_solares=require('./routes/panel_solar');
 
 // Ajustess 
@@ -12,8 +12,8 @@ app.use(express.json());
 // ajustess
 // defino middleware
 app.use('/api',paneles_solares);
-//app.use('/api/modulos',modulos);
-//app.use('/api/colegios',colegios);
+//app.use('/api/electrodomesticos',electrodomesticos); 
+//app.use('/api/usuario',usuario);
 
 
 app.listen(app.get('port'),()=>{
