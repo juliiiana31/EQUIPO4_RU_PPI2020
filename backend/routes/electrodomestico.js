e0f0b6342094ae07b622e8e3bf6644267f76fdd3
Laura Danniela Zárate <66334807+LauZar12@users.noreply.github.com>
2020-11-23 20:39:24 -0500

    Crear Put, Get succesfully
    Corregir el Post y el Delete

--- backend/routes/electrodomestico.js
+++ backend/routes/electrodomestico.js
@@ -2,30 +2,69 @@ const express =require('express');// tabnine
 const router= express.Router();
 const mysqlConnection =require('../db/db');
 
-// Colocar los middlewares  
-router.get('/electrodomesticos',(req,res)=>{
-    mysqlConnection.query('SELECT * from ELECTRODOMESTICOS',(err,rows,fiels)=>{
-    if(!err){
-       res.json(rows); 
-    }else{
-    console.log(err);
-    }});
-    })// fin
+   // Colocar los middlewares  
+router.get('/electrodomestico', (req, res) => {
+   mysqlConnection.query('SELECT * from ELECTRODOMESTICOS', (err, rows, fiels) => {
+      if (!err) {
+         res.json(rows);
+      } else {
+         console.log(err);
+      }
+   });
+})// fin
 
-    // traer un electrodomestico
+   //agregar un electrodomestico
+router.post('/electrodomestico/agregar', (req, res) => {
+   const { CODIGO, Nombre } = req.body;
+   let codigoArray = [ CODIGO, Nombre ];
+   let codigoNuevo = `INSERT INTO ELECTRODOMESTICOS ( CODIGO, Nombre ) VALUES (?,?)`;
+   mysqlConnection.query(codigoNuevo, codigoArray, (err, result, fields) => {
+      if (err) {
+         return console.error(err.message);
+      }
+      res.json({ message: 'Electrodomestico creado', })
+   });
+});
 
-    router.get('/electrodomestico/:codigo',(req,res)=>{
-        const { codigo } = req.params; 
-        mysqlConnection.query('SELECT * from ELECTRODOMESTICOS  where codigo = ?',[codigo], (err,rows,fiels)=>{
-        if(!err){
-           res.json(rows); 
-        }else{
-        console.log(err);
-        }});
-        })// fin
-
+// traer un electrodomestico
+router.get('/electrodomestico/:codigo', (req, res) => {
+   const { codigo } = req.params;
+   mysqlConnection.query('SELECT * from ELECTRODOMESTICOS  where CODIGO = ' + codigo, (err, rows, fiels) => {
+      if (!err) {
+         res.json(rows);
+      } else {
+         console.log(err);
+      }
+   });
+})// fin
         
-    
+    //actualizar electrodomestico
+router.put('/electrodomestico/:codigo', (req, res) => {
+   const { CODIGO, Nombre } = req.body;
+
+   const { codigo } = req.params;
+   mysqlConnection.query(`UPDATE ELECTRODOMESTICOS SET CODIGO=?, Nombre=? WHERE CODIGO = ?`,
+      [ CODIGO, Nombre, codigo ], (err, rows, fields) => {
+         if (!err) {
+            res.json({ status: 'Electrodomestico actualizado' });
+         } else {
+            console.log(err);
+         }
+      });
+});
+
+   //borrar el electrodomestico
+router.delete('/electrodomestico/:codigo', (req, res) => {
+   const { codigo } = req.params;
+   mysqlConnection.query('DELETE FROM ELECTRODOMESTICOS WHERE CODIGO= ?',
+      [codigo], (err, rows, fields) => { 
+         if (!err) {
+            res.json({ status: '¡Electrodomestico eliminado!' });
+         } else {
+            console.log(err);
+         }
+      });
+});
 
 module.exports =router;
 
